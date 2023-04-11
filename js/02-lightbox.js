1dd028dcbe29ca91f28bf791d0c47e4c9a932d84
TetianaKabanova <110104725+TetianaKabanova@users.noreply.github.com>
2023-04-11 15:17:19 +0200

    task 2 fixed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -10,32 +10,7 @@ const markupGallery = galleryItems
 
 listEl.insertAdjacentHTML("beforeend", markupGallery);
 
-listEl.addEventListener("click", onOpenModal);
-
-function onOpenModal(event) {
-  event.preventDefault();
-
-  const currentItem = event.target;
-
-  if (currentItem.nodeName !== "IMG") {
-    return;
-  }
-
-  const lightbox = new SimpleLightbox(".gallery .gallery__link", {
-    captionsData: "alt",
-    captionDelay: 250,
-  });
-
-  lightbox.open();
-  window.addEventListener("keydown", onCloseModal);
-}
-
-function onCloseModal(event) {
-  const openEl = document.querySelector(".basicLightbox");
-  if (event.code === "Escape") {
-    openEl.remove();
-  }
-  window.removeEventListener("keydown", onCloseModal);
-}
-
-console.log(galleryItems);
+const lightbox = new SimpleLightbox(".gallery .gallery__link", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
