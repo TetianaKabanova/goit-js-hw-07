8884c05d25bcaa10fc55a720037398cbff52b799
TetianaKabanova <110104725+TetianaKabanova@users.noreply.github.com>
2023-04-11 12:50:27 +0200

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const listEl = document.querySelector(".gallery");
+
+const markupGallery = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class='gallery__item'><a class='gallery__link' href='${original}'><img class='gallery__image' src='${preview}' data-source='${original}' alt='${description}'></a></li>`;
+  })
+  .join("");
+
+listEl.insertAdjacentHTML("beforeend", markupGallery);
+
+listEl.addEventListener("click", onOpenModal);
+
+function onOpenModal(event) {
+  event.preventDefault();
+
+  const currentItem = event.target;
+
+  if (currentItem.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.getAttribute(
+      "data-source"
+    )}" alt="${event.target.getAttribute("alt")}" />`
+  );
+
+  instance.show();
+  window.addEventListener("keydown", onCloseModal);
+}
+
+function onCloseModal(event) {
+  const openEl = document.querySelector(".basicLightbox");
+  if (event.code === "Escape") {
+    openEl.remove();
+  }
+  window.removeEventListener("keydown", onCloseModal);
+}
 
 console.log(galleryItems);
